8064252b170742fae9856fcc4bf0c161c4fa4715
Rishita Mukherjee <rishitam2701@gmail.com>
2021-02-22 22:39:42 +0530

    done till 7

--- app/src/main/java/chatting/app/expressify/MainActivity.java
+++ app/src/main/java/chatting/app/expressify/MainActivity.java
@@ -2,21 +2,73 @@ package chatting.app.expressify;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager.widget.ViewPager;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar mtoolbar;
+    private ViewPager myViewPager;
+    private TabLayout myTabLayout;
+    private TabsAccessorAdapter myTabsAccessorAdapter;
+
+    private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
+    private DatabaseReference RootRef;
+    private String currentUserID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        mAuth = FirebaseAuth.getInstance();
+//        currentUser = mAuth.getCurrentUser();
+//        currentUserID = mAuth.getCurrentUser().getUid();
+//        RootRef = FirebaseDatabase.getInstance().getReference();
+
         mtoolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
         setSupportActionBar(mtoolbar);
         getSupportActionBar().setTitle("Expressify");
 
+        myViewPager = (ViewPager) findViewById(R.id.main_tabs_pager);
+        myTabsAccessorAdapter = new TabsAccessorAdapter(getSupportFragmentManager());
+        myViewPager.setAdapter(myTabsAccessorAdapter);
+        myTabLayout = (TabLayout) findViewById (R.id.main_tabs);
+        myTabLayout.setupWithViewPager(myViewPager);
+    }
+    @Override
+    protected void onStart()
+    {
+        super.onStart();
+
+        if (currentUser == null)
+        {
+            SendUserToLoginActivity();
+        }
+//        else
+//        {
+//            updateUserStatus("online");
+//
+//            VerifyUserExistance();
+//        }
+    }
+
+    private void SendUserToLoginActivity()
+    {
+        Intent loginIntent = new Intent(MainActivity.this, LoginActivity.class);
+        loginIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        startActivity(loginIntent);
 
     }
 }
